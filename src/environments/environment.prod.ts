fc6f45a5b0a6fa37a3bcbedabd3683c33a065e70
Dimitar Filipov <grinndev@protonmail.com>
2021-09-27 15:25:58 +0300

    update .gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://localhost:4003/v1',
-  apiTOKEN: "607f1689bb251",
-  collectionTOKEN: "607f39b3abaa1"
+  API_URL: 'https://localhost:4003/v1',
+  PROJECT_KEY: "607f1689bb251",
+  COLLECTION_KEY: "607f39b3abaa1"
 };
