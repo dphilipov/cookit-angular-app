95fadf58d5bed9798104727e3b6c420c9801f5dd
Dimitar Filipov <grinndev@protonmail.com>
2021-09-28 20:53:59 +0300

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://localhost:4003/v1',
-  PROJECT_KEY: "607f1689bb251",
-  COLLECTION_KEY: "607f39b3abaa1"
+  API_URL: 'https://appwrite.cookitapp.org/v1',
+  PROJECT_KEY: "6151f29fd2492",
+  COLLECTION_KEY: "6151fb0bda744"
 };
