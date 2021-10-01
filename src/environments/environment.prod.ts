e6acf6e497395c53fedea78330ac79b8d410b511
Dimitar Filipov <grinndev@protonmail.com>
2021-10-01 17:34:18 +0300

    upodate environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://appwrite.cookitapp.org/v1',
-  PROJECT_KEY: "6151f29fd2492",
-  COLLECTION_KEY: "6151fb0bda744"
+  PROJECT_KEY: "61562f031f2b8",
+  COLLECTION_KEY: "6156368015f14"
 };
